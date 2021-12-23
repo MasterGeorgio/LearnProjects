b5e4413982d1db5cc3df0ef1a418e6e528e90bfb
George <ssalimovgorikk@gmail.com>
2021-12-23 22:25:47 +0300

    Solve L8 t3

--- L8/Lesson8.cpp
+++ L8/Lesson8.cpp
@@ -4,12 +4,16 @@
 #include "stdafx.h"
 
 #include <iostream>
+#include <map>
 #include <string>
 
 //#include "BJack.h"
 
 using namespace std;
 
+
+// ------
+// Task 1
 template<typename T>
 double div(T a, T b) {
 	if (!b)
@@ -19,6 +23,8 @@ double div(T a, T b) {
 }
 
 
+// ------
+// Task 2
 class Ex
 {
 public:
@@ -52,16 +58,108 @@ private:
 	double m_y;
 };
 
+// ------
+// Task 3
+class Robot;
 
+enum eMove {
+	Stop,
+	Left,
+	Rigth,
+	Up,
+	Down
+};
+
+std::map<eMove, const char*> g_mapMove = {
+	{ Left,  "Left" },
+	{ Rigth, "Rigth" },
+	{ Up,    "Up" },
+	{ Down,  "Down" }
+};
+
+class OffTheField
+{
+public:
+	OffTheField(eMove move, int x, int y) try :
+		m_move(move),
+		m_x(x),
+		m_y(y) {
+		throw 1;
+	}
+	catch (int n)
+	{
+		cerr << "Exeption OffTheField" << endl;
+		cerr << "move: " << g_mapMove[move] << " X: " << x << " Y: " << y << endl;
+	}
+
+	~OffTheField() {}
+private:
+	int   m_x;
+	int   m_y;
+	eMove m_move;
+};
+
+class IllegalCommand
+{
+public:
+	 IllegalCommand(eMove move, int x, int y) try :
+		m_move(move),
+		m_x(x),
+		m_y(y) {
+		throw 1;
+	}
+	catch (int n)
+	{
+		cerr << "Exeption IllegalCommand" << endl;
+		cerr << "move: " << g_mapMove[move] << " X: " << x << " Y: " << y << endl;
+	}
+
+	~IllegalCommand() {}
+private:
+	int   m_x;
+	int   m_y;
+	eMove m_move;
+};
+
+const int g_nSize = 10;
 
 class Robot
 {
 public:
-	Robot() {}
+	Robot() : m_x(0), m_y(0), m_move(Down) {}
 	~Robot() {}
 
-private:
+	void Move(eMove move, int step) {
+		m_move = move;
+		if (m_move == Left)
+			Shift(m_x, -step);
+		else if (m_move == Rigth)
+			Shift(m_x, step);
+		else if (m_move == Up)
+			Shift(m_y, -step);
+		else if (m_move == Down)
+			Shift(m_y, step);
+		else if (m_move == Stop)
+			cout << "Stop robot" << endl;
+		else
+			throw IllegalCommand::IllegalCommand(move, m_x, m_y);
 
+		cout << "move: " << g_mapMove[move] << " X: " << m_x << " Y: " << m_y << endl;
+	}
+
+private:
+	void Shift(int &nCoord, int step) {
+		if ((nCoord + step > g_nSize) ||
+			(nCoord + step < 0))
+			throw OffTheField::OffTheField(m_move, m_x, m_y);
+
+		nCoord += step;
+	}
+
+private:
+	int   m_x;
+	int   m_y;
+	eMove m_move;
 };
 
 
@@ -75,6 +173,7 @@ void main()
 		cerr << exeption << endl;
 	}
 
+	// Task 2
 	Bar bar;
 	int nParam = 0;
 	do {
@@ -87,6 +186,23 @@ void main()
 
 		}
 
+		cin.clear();
+
+	} while (nParam);
+
+	// Task 3
+	Robot robot;
+	do {
+		cout << "Robot:" << endl;
+		cout << "0 - exit:" << endl;
+		cout << "1 - Left:" << endl;
+		cout << "2 - Rigth:" << endl;
+		cout << "3 - Up:" << endl;
+		cout << "4 - Down:" << endl;
+		cout << "Enter step:" << endl;
+
+		cin >> nParam;
+		robot.Move(static_cast<eMove>(nParam), 1);
 		cin.clear();
 
 	} while (nParam);
